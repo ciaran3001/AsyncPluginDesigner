c31a1d688e8165e93f669c23b231e8c5903d62d0
Ciaran Kelly <Ciaran.Kelly@ergogroup.ie>
2023-03-06 21:35:36 +0000

    initial commit for plugin assembly

---
+++ PluginDesigner/Models/DefineVariables/BooleanVariable.cs
@@ -0,0 +1,27 @@
+﻿using PluginDesigner.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginDesigner.Models.DefineVariables
+{
+    class BooleanVariable : IWorkflowAction
+    { 
+        public bool? Value { get; set; }
+
+
+        ReturnType IWorkflowAction.ReturnType()
+        {
+            return ReturnType.String;
+        }
+
+        public bool Execute()
+        {
+            return true;
+        }
+
+
+    }
+}
---
+++ PluginDesigner/Models/WorkflowAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginDesigner.Models
+{
+    class WorkflowAction
+    {
+        string ActionType { get; set; }
+
+        object[] Properties = new object[3];
+    }
+}
